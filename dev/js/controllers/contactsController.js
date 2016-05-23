b1b3c8aa0df021ebb6b42f7a99a9474d3bcc7661
Luke Harrison <luke.harrison@plus.net>
2016-05-23 11:14:03 +0100

    dev

--- dev/js/controllers/contactsController.js
+++ dev/js/controllers/contactsController.js
@@ -1,10 +1,34 @@
 "use strict"
 
-module.exports = angular.module("app", []).controller("contactsController", function(){
-	var data = require("../../data/contacts");
-	this.contacts = data;
+module.exports = angular.module("app", []).controller("contactsController", function($scope){
 
-	for(var i = 0; i < this.contacts.length; i++) {
-		this.contacts[i].fullName = this.contacts[i].firstName + " " + this.contacts[i].lastName;
+	// var data = require("../../data/contacts");
+	// localStorage.setItem('contacts', JSON.stringify(data));
+
+	if(localStorage.getItem("contacts")) {
+		this.contacts = JSON.parse(localStorage.getItem('contacts'));
+
+		for(var i = 0; i < this.contacts.length; i++) {
+			this.contacts[i].fullName = this.contacts[i].firstName + " " + this.contacts[i].lastName;
+		}
+
 	}
+
+	this.contactForm = {};
+
+	this.contactForm.submit = function(){
+		console.log("submit");
+
+		// Create session object in scope
+		this.contactForm.session = {};
+
+	}
+
+	this.contactForm.clear = function(){
+		console.log("cleared");
+		$("#add-contact").reset();
+	};
+
+	console.log($scope);
+
 });
