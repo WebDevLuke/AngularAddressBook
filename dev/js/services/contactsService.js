3cc22193c53f7b691fee2963e581cb5c4bc178ae
Luke Harrison <luke.harrison@plus.net>
2016-05-25 11:55:36 +0100

    Fixed merge error

--- dev/js/services/contactsService.js
+++ dev/js/services/contactsService.js
@@ -4,47 +4,34 @@ module.exports = function(){
 
 	var service = { 
 
-		contacts: {},
+		contacts: [], 
 
 		getContacts: function(){
-			// Get from local storage if exists
 			if(localStorage.getItem("contacts")) {
-				var contactsList = JSON.parse(localStorage.getItem('contacts'));	
-				this.contacts = contactsList;
+				this.contacts = JSON.parse(localStorage.getItem('contacts'));
+				return this.contacts;
 			}
-
-			console.log(this.contacts);
-
 		},
  
 		addNewContact: function(formData) {
 			// Create fullName parameter
 			formData.fullName = formData.firstName + " " + formData.lastName;
 
-			var firstLetter = formData.firstName.substring(0,1).toUpperCase();
-			if(!this.contacts[firstLetter]) {
-				this.contacts[firstLetter] = [];
-			}
-			this.contacts[firstLetter].push(formData);
+			// Push new data to contacts
+			this.contacts.push(formData);
+
+			console.log(this.contacts);
 
 			// Push to local storage
 			localStorage.setItem('contacts', JSON.stringify(this.contacts));
 		},
 
-<<<<<<< HEAD
-		deleteContact: function(index) {
-			this.contacts.splice(index, 1);
-
-			// Push to local storage
-			localStorage.setItem('contacts', JSON.stringify(this.contacts));
-=======
 		deleteContact: function(item) {
 
 			this.contacts.splice(this.contacts.indexOf(item),1);
 
 			// Push to local storage
 			//localStorage.setItem('contacts', JSON.stringify(this.contacts));
->>>>>>> develop
 		}
 
 	}
